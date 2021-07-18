91d1aad7e7704e4bf3e7ac08a68aad721f57f8db
Eric Syu <xeriok02390@gmail.com>
2021-07-18 23:50:05 +0800

    create team model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,8 @@
 class Player < ApplicationRecord
-  enum dominant_hand: { 'left_hand'=> 0, 'right_hand'=> 1, 'both'=> 2 }
-  enum weather: { 'sunny'=> 0, 'cloudy'=> 1, 'rain'=> 2, 'ice'=> 3 }
+  enum dominant_hand: { 'left'=> 0, 'right'=> 1, 'both'=> 2 }
+  enum weather: { 'sunny'=> 0, 'cloudy'=> 1, 'rain'=> 2, 'snow'=> 3 }
   enum rank: { 'blue'=> 0, 'yellow'=> 1, 'red'=> 2, 'gold'=> 3, 'purple'=> 4, 'black'=> 5 }
-  enum card_type: { 'batter'=> 0, 'pittcher'=> 1, 'coach'=> 2 }
+  enum play_type: { 'batter'=> 0, 'pittcher'=> 1, 'coach'=> 2 }
+
+  has_one :team
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_153818) do
+ActiveRecord::Schema.define(version: 2021_07_18_154302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2021_07_18_153818) do
     t.integer "rank"
     t.integer "play_type"
     t.integer "team_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
